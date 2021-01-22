c30cbc2c58f520bfcf6512b4365024407632877e
wenlianghuang <wenlianghuang@gmail.com>
2021-01-22 12:18:07 +0800

    20210122 modified file

--- src/index.js
+++ src/index.js
@@ -81,6 +81,3 @@ ReactDOM.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-import Amplify from 'aws-amplify';
-import config from './aws-exports';
-Amplify.configure(config);
